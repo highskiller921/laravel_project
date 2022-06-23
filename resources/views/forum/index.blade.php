6f6ee2562df3fe8def80d080897d8c1646ffa5ee
phn921 <panghenam@gmail.com>
2022-06-24 04:38:24 +0800

    reply_count

--- resources/views/forum/index.blade.php
+++ resources/views/forum/index.blade.php
@@ -31,10 +31,16 @@
 
                 <li class="list-group-item list-group-item-action">
                   <a href="{{url('/')}}/{{$post->id}}/view" style="text-decoration: none" class="text-dark">{{$post->title}}</a>
-                    <span class="badge bg-info text-dark"><i class="fa-solid fa-comment-dots"></i></i> 4</span>
+                    <span class="badge bg-info text-dark"><i class="fa-solid fa-comment-dots"></i></i>
+                         {{App\Models\Reply::where('post_id', $post->id)->count()}}</span>
                     <span class="badge bg-danger"><i class="fa-solid fa-heart"></i> 3</span>
                     <br>
-                    <small>{{$post->created_at}} | by SB Hero</small>
+                    <small>{{$post->created_at}} | by 
+                        @php
+                         $user=App\Models\User::find($post->user_id);
+                        @endphp
+                         {{$user->name}}
+                    </small>
                 </li>
                 @endforeach
             </ul>
